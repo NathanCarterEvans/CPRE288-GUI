b67b61f6d4e4f620f1b0c9606fd8643016276fd5
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-04-28 14:04:55 -0500

    send scan data

--- dummy_data.py
+++ dummy_data.py
@@ -9,15 +9,21 @@ s.bind((HOST, PORT))
 s.listen(5)
 print("Server is listening on {}:{}".format(HOST, PORT))
 
+# pub angle: i32, // 90 degrees is forward, 180 is left, 0 is right
+# pub sound: f32, // cm
+# pub ir: i32,
+angle = 0
 while True:
     client_socket, client_address = s.accept()
     print("Connection from:", client_address)
 
     with client_socket:
         while True:
+            angle = (angle + 5) % 180
+            data = f'{{ "angle": {angle}, "sound": 98.4, "ir": 0 }}'
             try:
-                client_socket.sendall(b"data!!!")
-                time.sleep(2)
+                client_socket.sendall(data.encode())
+                time.sleep(0.5)
             except BrokenPipeError:
                 print('connection closed')
                 break
