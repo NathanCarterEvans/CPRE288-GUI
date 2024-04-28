5cf5af05a64d19b8802672a0feb5c6740b9b5adf
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-04-28 12:53:19 -0500

    copy my old ui

---
+++ src/cybot.rs
@@ -0,0 +1,49 @@
+use macroquad::prelude::*;
+use serde::{Deserialize, Serialize};
+
+pub const CYBOT_RADIUS: f32 = 15_f32;
+pub const SOUND_THICKNESS: f32 = 20.0;
+pub const IR_THICKNESS: f32 = 5.0;
+pub const SOUND_COLOR: Color = BLUE;
+pub const IR_COLOR: Color = RED;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CybotScanData {
+    pub angle: i32, // 90 degrees is forward, 180 is left, 0 is right
+    pub sound: f32, // cm
+    pub ir: i32,
+}
+impl CybotScanData {
+    fn get_end_pt(&self, dist: f32, start_x: f32, start_y: f32) -> (f32, f32) {
+        let deg = (self.angle - 90) * -1;
+        let x = start_x + (dist * (deg as f32).to_radians().cos());
+        let y = start_y + (dist * (deg as f32).to_radians().sin());
+
+        (x, y)
+    }
+    fn draw_scan_data(&self, start_x: f32, start_y: f32) {
+        let (end_x, end_y) = self.get_end_pt(self.sound, start_x, start_y);
+        draw_line(start_x, start_y, end_x, end_y, SOUND_THICKNESS, SOUND_COLOR);
+        let (end_x, end_y) = self.get_end_pt(self.ir as f32, start_x, start_y);
+        draw_line(start_x, start_y, end_x, end_y, IR_THICKNESS, IR_COLOR);
+    }
+}
+pub struct Cybot {
+    x: f32,
+    y: f32,
+    pub data: Option<CybotScanData>,
+}
+
+impl Cybot {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y, data: None }
+    }
+    pub fn draw_bot(&self) {
+        draw_circle(self.x, self.y, CYBOT_RADIUS, YELLOW);
+        if let Some(d) = &self.data {
+            d.draw_scan_data(self.x, self.y);
+        }
+    }
+
+    pub fn move_bot(&mut self, distance: f32, deg: f32) {}
+}
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+pub mod cybot;
+
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+use cybot::{Cybot, CybotScanData};
+use macroquad::prelude::*;
+
+#[macroquad::main("BasicShapes")]
+async fn main() {
+    let mut stream = TcpStream::connect("192.168.1.1:288").expect("error connecting to host");
+    let mut buf = [0_u8; 200];
+
+    loop {
+        clear_background(BLACK);
+        let bytes_read = stream.read(&mut buf).expect("reading error");
+        println!("read {bytes_read:?} bytes");
+        let msg = match serde_json::from_slice::<CybotScanData>(&buf[..bytes_read]) {
+            Ok(data) => {
+                let h = screen_height();
+                let w = screen_width();
+                // Draw robot
+                let mut bot = Cybot::new(w / 2., h / 2.);
+                // println!("{}", ser9e_json::to_string(&scan).unwrap());
+                let s = format!("data: \n{data:?}");
+                bot.data = Some(data);
+                bot.draw_bot();
+                s
+            }
+            Err(_) => {
+                let s = String::from_utf8_lossy(&buf[..bytes_read]);
+
+                s.to_string()
+            }
+        };
+        if let Some(c) = get_char_pressed() {
+            stream.write(&[c as u8]).expect("error writing");
+        }
+
+        println!("{}", msg);
+
+        // draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        // draw_rectangle(w/ 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        // draw_circle(w - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        // draw_text("HELLO", 20.0, 20.0, 20.0, DARKGRAY);
+
+        // draw_circle(w/2.,h/2., 14., YELLOW);
+        next_frame().await
+    }
+}
+
+// fn main() {
+//     let mut stream = TcpStream::connect("192.168.1.1:288").expect("error connecting to host");
+//     let mut buf = [0_u8; 200];
+//     loop {
+//     }
+// }
