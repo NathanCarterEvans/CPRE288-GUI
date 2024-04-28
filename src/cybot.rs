b7fe41c4479d216fa5058fd53d17e2fbbcab85b0
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-04-28 14:08:13 -0500

    rotate field by 90 degrees ccw

--- src/cybot.rs
+++ src/cybot.rs
@@ -15,7 +15,7 @@ pub struct CybotScanData {
 }
 impl CybotScanData {
     fn get_end_pt(&self, dist: f32, start_x: f32, start_y: f32) -> (f32, f32) {
-        let deg = (self.angle - 90) * -1;
+        let deg = (self.angle) * -1;
         let x = start_x + (dist * (deg as f32).to_radians().cos());
         let y = start_y + (dist * (deg as f32).to_radians().sin());
 
