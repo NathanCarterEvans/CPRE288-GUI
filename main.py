d313f9f231830d39194e92fcfe2aa1d2d0816321
NathanCcArter <natec.carter15@gmail.com>
2024-05-01 14:45:59 -0500

    .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ root = tk.Tk()
 root.title("CyBot Radar")
 
 # Create a canvas for drawing
-canvas = tk.Canvas(root, width=800, height=800, bg='white')
+canvas = tk.Canvas(root, width=800, height=710, bg='white')
 canvas.pack()
 
 # Draw the radar
@@ -94,7 +94,7 @@ def connect_to_cybot():
     global client_socket
     try:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # client_socket.settimeout(3)
+        client_socket.settimeout(3)
         client_socket.connect(('192.168.1.1', 288))
         print("connected!")
         threading.Thread(target=receive_messages, daemon=True).start()
@@ -115,19 +115,30 @@ def send_command(command):
 connect_button = tk.Button(root, text="Connect to CyBot", command=connect_to_cybot)
 connect_button.pack(pady=10)
 
+sound_button = tk.Button(root, text="Play sound", command=lambda: send_command('r'))
+sound_button.pack(pady=10)
+
 button_up = tk.Button(root, text="Up", command=lambda: send_command('w'))
 button_up.pack(side='top')
+
 button_down = tk.Button(root, text="Down", command=lambda: send_command('s'))
 button_down.pack(side='bottom')
+
 button_left = tk.Button(root, text="Turn Left", command=lambda: send_command('a'))
 button_left.pack(side='left')
+
 button_right = tk.Button(root, text="Turn Right", command=lambda: send_command('d'))
 button_right.pack(side='right')
+
 button_scan = tk.Button(root, text="Scan", command=lambda: [clear_scan(), send_command('q')])
 button_scan.pack(pady=10)
+
 text_input = tk.Text(root, height=1, width=5)
 text_input.pack()
+
 send_button = tk.Button(root, text="send cmd", command=lambda: send_command(text_input.get(1.0, 2.0).strip()))
+
 send_button.pack()
+
 connect_to_cybot()
 root.mainloop()
